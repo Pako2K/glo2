565ff7aadf7b550a2400b263f8e26eccc893716e
Pako2K <41144260+Pako2K@users.noreply.github.com>
2018-08-13 00:54:40 +0200

    Initial upload

---
+++ src/program.cpp
@@ -0,0 +1,93 @@
+//  MIT License
+//
+//  Copyright (c) 2018 Francisco de Lanuza
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#include "glo2/program.h"
+
+#include <vector>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+
+using namespace glo2;
+
+Program::Program(){
+  _programID = glCreateProgram();
+}
+
+
+Program::~Program(){
+  glDeleteProgram(_programID);
+}
+
+
+void Program::attachShader(Shader &a_shader){
+  glAttachShader(_programID, a_shader.id());
+}
+
+
+void Program::linkProgram() throw(std::string){
+  glLinkProgram(_programID);
+
+	// Check the program
+	GLint result{ GL_FALSE };
+	int info_log_length;
+	glGetProgramiv(_programID, GL_LINK_STATUS, &result);
+	glGetProgramiv(_programID, GL_INFO_LOG_LENGTH, &info_log_length);
+	if (info_log_length > 0) {
+		std::vector<char> error_message(info_log_length + 1);
+		glGetProgramInfoLog(_programID, info_log_length, NULL, &error_message[0]);
+
+		if (result == GL_FALSE) {
+      throw std::string("Error while linking programm.\Error: ") + std::string(&error_message[0]);
+    }
+	}
+
+	_isLinked = true;
+
+	// Detach shaders
+	GLint shaders_count;
+	glGetProgramiv(_programID, GL_ATTACHED_SHADERS, &shaders_count);
+	std::vector<GLuint> shader_ids(shaders_count);
+  glGetAttachedShaders(_programID, shaders_count, &shaders_count, &shader_ids[0]);
+	for (auto shader : shader_ids) {
+		glDetachShader(_programID, shader);
+	}
+}
+
+GLuint Program::id() const{
+  return _programID;
+}
+
+bool Program::isLinked() const{
+  return _isLinked;
+}
+
+
+void Program::useProgram() const{
+  glUseProgram(_programID);
+}
+
+
+void Program::releaseProgram() const{
+  glUseProgram(0);
+}
---
+++ src/shader.cpp
@@ -0,0 +1,76 @@
+//  MIT License
+//
+//  Copyright (c) 2018 Francisco de Lanuza
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#include "glo2/shader.h"
+
+#include <fstream>
+#include <vector>
+
+using namespace glo2;
+
+Shader::Shader(GLenum type) {
+  _shaderID = glCreateShader(type);
+}
+
+Shader::~Shader() {
+  glDeleteShader(_shaderID);
+}
+
+GLuint Shader::id() const {
+  return _shaderID;
+}
+
+void Shader::loadSource(const std::string &shader_file) throw(std::string) {
+  // Read the shader code from the file
+  std::string shader_code;
+  std::ifstream shader_stream (shader_file, std::ios::in);
+  if (shader_stream.is_open()) {
+    std::string line = "";
+    while (getline(shader_stream, line))
+      shader_code += "\n" + line;
+    shader_stream.close();
+  }
+  else {
+    throw std::string("Cannot open shader file: " + shader_file);
+  }
+
+  loadSource(shader_code.c_str());
+}
+
+void Shader::loadSource(const char *shader_str) throw(std::string) {
+  glShaderSource(_shaderID, 1, &shader_str, NULL);
+  glCompileShader(_shaderID);
+
+  // Check Shader
+  GLint result{ GL_FALSE };
+  int info_log_length;
+  glGetShaderiv(_shaderID, GL_COMPILE_STATUS, &result);
+  glGetShaderiv(_shaderID, GL_INFO_LOG_LENGTH, &info_log_length);
+  if (info_log_length > 0) {
+    std::vector<char> error_message(info_log_length + 1);
+    glGetShaderInfoLog(_shaderID, info_log_length, NULL, &error_message[0]);
+
+    if (result == GL_FALSE) {
+      throw std::string("Error while compiling shader:\n" + std::string(shader_str) + "\nCompilation error: " + &(error_message[0]) );
+    }
+  }
+}
